835ddc3a28fa70bf520432a2f7239dffb7944817
RF212690 <90518866+RF212690@users.noreply.github.com>
2022-03-20 22:03:02 +0100

    Adds menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,29 @@
 package com.company;
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class Main {
 
     public static void main(String[] args) {
         Screen gameScreen= new Screen();
         Panel gamePanel=new Panel();
+        ImageIcon button=new ImageIcon("button.png");
+        JButton b1 = new JButton(button);
+        b1.setVerticalTextPosition(AbstractButton.CENTER);
+        b1.setHorizontalTextPosition(AbstractButton.CENTER);
         gameScreen.add(gamePanel);
+        gameScreen.add(new JLabel("GameTitle"), BorderLayout.NORTH);
         gameScreen.pack();
         gameScreen.setLocationRelativeTo(null);
         gameScreen.setVisible(true);
-        gamePanel.StartThread();
+        b1.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e)
+            {
+                gamePanel.StartThread();
+            }
+        });
     }
 }
