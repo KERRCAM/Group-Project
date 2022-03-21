f07365b56c35940154f889d261f64496eb5a9ae2
oh210602 <OH210602@reigate.ac.uk>
2022-03-21 10:51:30 +0000

    random map done

--- src/com/company/mapCreating.java
+++ src/com/company/mapCreating.java
@@ -9,31 +9,40 @@ public class mapCreating {
     private static File RandomGameMap = new File("RandomGameMap.txt");
 
     public static void CreateMap(){
+        File RandomGameMap = new File("RandomGameMap.txt");
         Random random = new Random();
-        int mapX = random.nextInt(30)+20;
-        int mapY = random.nextInt(30)+20;
+        int mapX = random.nextInt(50)+100;
+        int mapY = random.nextInt(50)+100;
         try{
             FileWriter myWriter = new FileWriter(RandomGameMap.getName(),true);
             for (int i = 0; i < mapY; i++) {
                 myWriter.write("5 ");
                 for (int j = 0; j < mapX; j++) {
-                    int platformChance = random.nextInt(5);
-                    if (platformChance == 1){
+                    int platformChance = random.nextInt(15);
+                    if (platformChance == 1 && j < mapX-3){
                         myWriter.write("1 0 2 ");
-                        i++;
-                        i++;
+                        j++;
+                        j++;
+
                     }else{
                         myWriter.write("4 ");
                     }
                 }
-                myWriter.write("5 ");
+                myWriter.write("5 \n");
 
             }
-
+            myWriter.close();
         }catch(Exception e){
             System.out.println("error:"+e);
         }
 
+    }
+    public static void DeleteRandomMapFile(){
+        if (RandomGameMap.delete()) {
+            System.out.println("Deleted the file: " + RandomGameMap.getName());
+        } else {
+            System.out.println("Failed to delete the file.");
+        }
     }
 
 
